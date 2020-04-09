59173376d63d39f5779da9bd8801c6674f906096
Jacktks <30370563+Jacktks@users.noreply.github.com>
2020-04-09 21:00:15 +0700

    git add Cart-item component and edit cart-dropdown component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 
 import './cart-dropdown.style.scss';
 
-import CuttomButton from '../custom-button/custom-button.component';
+import {connect} from 'react-redux'; 
 
-const CartDropdown = () => (
+import CuttomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+
+const CartDropdown = ({cartItems}) => (
     <div className="cart-dropdown">
-        <div className="cart-items"/>
+        <div className="cart-items">
+            {
+                cartItems.map(cartItems => <CartItem key={cartItems.id} item={cartItems}></CartItem>)
+            }
+        </div>
         <CuttomButton>GO TO CHECK OUT!</CuttomButton>
     </div>
 )
 
-export default CartDropdown;
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown);
