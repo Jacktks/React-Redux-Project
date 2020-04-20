d6692ee8c880eab837a3820c421e06bd946a7e22
Jacktks <30370563+Jacktks@users.noreply.github.com>
2020-04-20 23:04:33 +0700

    removing redux-loger from production build

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route exact path='/checkout' render={() => this.props.currentUser ? (<CheckoutPage/>) : (<SignInAndSignUp/>)}/>
           <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to="/"/>) : (<SignInAndSignUp/>)} />
         </Switch>
       </div>
